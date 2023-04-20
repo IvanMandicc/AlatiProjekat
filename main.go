48b75370ef0c3eafed95eb754450cfde004b011a
IvanMandicc <mandiccivan@gmail.com>
2023-04-20 13:10:51 +0200

    dodate handler metode u main.go

--- main.go
+++ main.go
@@ -1,7 +1,60 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-    fmt.Println("Hello World")
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	router := mux.NewRouter()
+	router.StrictSlash(true)
+
+	server := configServer{
+		data:      map[string]*Config{},
+		dataGroup: map[string]*Group{},
+	}
+	router.HandleFunc("/config/", server.createConfigHandler).Methods("POST")
+	router.HandleFunc("/configs/", server.getAllHandler).Methods("GET")
+	router.HandleFunc("/config/{id}/", server.getConfigHandler).Methods("GET")
+	router.HandleFunc("/config/{id}/", server.delConfigHandler).Methods("DELETE")
+	router.HandleFunc("/group/", server.createGroupHandler).Methods("POST")
+	router.HandleFunc("/group/{groupId}/config{id}/", server.AddConfigToGroup).Methods("PUT")
+	router.HandleFunc("/groups/", server.getAllHandler).Methods("GET")
+	router.HandleFunc("/group/{id}/", server.getGroupHandler).Methods("GET")
+	router.HandleFunc("/group/{id}/", server.delGroupHandler).Methods("DELETE")
+	router.HandleFunc("/group/{groupId}/config/{id}/", server.delConfigFromGroupHandler).Methods("DELETE")
+
+	// start server
+	srv := &http.Server{Addr: "0.0.0.0:8000", Handler: router}
+	go func() {
+		log.Println("server starting")
+		if err := srv.ListenAndServe(); err != nil {
+			if err != http.ErrServerClosed {
+				log.Fatal(err)
+			}
+		}
+	}()
+
+	<-quit
+
+	log.Println("service shutting down ...")
+
+	// gracefully stop server
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("server stopped")
 }
