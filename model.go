42810d3590cb84a552ae21ddc698405f80c68815
mandicaleksa <101620804+mandicaleksa@users.noreply.github.com>
2023-04-20 13:35:11 +0200

    Dodati server.go, model.go i helper.go. U model.go dodate strukture config, service i group. U server.go dodati handleri za config i group.

---
+++ model.go
@@ -0,0 +1,18 @@
+package main
+
+type Config struct {
+	Id      string            `json:"id"`
+	Entries map[string]string `json:"entries"`
+	//Title string   `json:"title"`
+	//Text  string   `json:"text"`
+	//Tags  []string `json:"tags"`
+}
+
+type Service struct {
+	Data map[string]*[]Config
+}
+
+type Group struct {
+	Id      string   `json:"id"`
+	Configs []Config `json:"configs"`
+}
